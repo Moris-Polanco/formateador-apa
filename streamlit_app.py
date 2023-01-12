c3d405b2979818132369626f0d2ef9a7db8c0fc1
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-11 18:34:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,8 @@ def format_reference(reference):
     openai_prompt = (f"Format the following reference in APA style: "
                      f"{reference.authors} ({reference.year}). {reference.title}. {reference.source}")
     completions = openai.Completion.create(engine="text-davinci-002", prompt=openai_prompt, max_tokens=1024, n=1,stop=None,temperature=0.5)
-    message = completions.choices[return completions.choices[0].text
+    message = completions.choices[
+        return completions.choices[0].text
 
 def run_app():
     """Run the Streamlit app.
