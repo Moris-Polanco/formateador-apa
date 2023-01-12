d03d23b7d4fc807b8ac060dcdf99b250a82790ec
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-11 18:58:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,34 +19,50 @@ class Reference:
 
 # Función para leer un archivo RIS y convertirlo en una lista de referencias
 def read_ris_file(file):
+    """Read a RIS file and return a list of references.
+    Parameters:
+        file (UploadedFile): The UploadedFile object returned by st.file_uploader.
+    Returns:
+        List[Reference]: A list of named tuples representing references.
+    """
     references = []
     file_data = io.StringIO(file.getvalue().decode("utf-8"))
     with file_data as f:
         lines = f.readlines()
         reference = ""
+        reference_type = ""
+        authors = ""
+        year = ""
+        title = ""
+        source = ""
         for line in lines:
             if line.startswith("TY"):
-                match = re.search(r"TY\s+(.*)\n", reference)
+                match = re.search(r"TY\s+(.*)\n", line)
                 reference_type = match.group(1) if match else ""
             elif line.startswith("AU"):
-                match = re.search(r"AU\s+(.*)\n", reference)
+                match = re.search(r"AU\s+(.*)\n", line)
                 authors = match.group(1) if match else ""
             elif line.startswith("PY"):
-                match = re.search(r"PY\s+(\d{4})\n", reference)
+                match = re.search(r"PY\s+(\d{4})\n", line)
                 year = match.group(1) if match else ""
             elif line.startswith("TI"):
-                match = re.search(r"TI\s+(.*)\n", reference)
+                                match = re.search(r"TI\s+(.*)\n", line)
                 title = match.group(1) if match else ""
             elif line.startswith("T2"):
-                match = re.search(r"T2\s+(.*)\n", reference)
+                match = re.search(r"T2\s+(.*)\n", line)
                 source = match.group(1) if match else ""
             elif line.startswith("ER"):
                 # Parse reference
                 references.append(Reference(type=reference_type, authors=authors, year=year, title=title, source=source))
-                reference = ""
+                reference_type = ""
+                authors = ""
+                year = ""
+                title = ""
+                source = ""
             else:
                 reference += line
     return references
+
 
 # Función para usar GPT-3 para dar formato a una referencia en estilo APA
 def format_reference(reference):
